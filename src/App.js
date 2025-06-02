9b7326fb005753a62881f5ea9e0b9ae1be76d948
tejonidhi-wizzy <tejonidhi@wizzybox.com>
2025-06-02 18:57:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import UserForm from './components/UserForm';
+import UserList from './components/UserList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav style={{ padding: "10px" }}>
+        <Link to="/" style={{ marginRight: "15px" }}>Register</Link>
+        <Link to="/users">View Users</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<UserForm />} />
+        <Route path="/users" element={<UserList />} />
+        <Route path="/edit/:id" element={<UserForm />} />
+      </Routes>
+    </Router>
   );
 }
 
